0c8e98be52b94b5644deea67022a614ae5e01e5c
Benjamin MANGUELE <benjaminmanguele7@gmail.com>
2022-05-31 12:31:28 +0200

    update contrat

--- nounous-web/src/main/java/nounous/jsf/data/Contrat.java
+++ nounous-web/src/main/java/nounous/jsf/data/Contrat.java
@@ -8,6 +8,7 @@ import java.util.Timer;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 
 @SuppressWarnings("serial")
@@ -32,7 +33,27 @@ public class Contrat implements Serializable {
 	
 	private Parent parent;
 	
-	private Garder garder;
+	@NotNull( message = "La date est obligatoire")
+	private Date		DateDeGarde;
+     
+	
+	
+	private Timer heureArrivee;
+	
+	private Timer heureDepart;
+	
+	private boolean repas;
+	
+	@NotBlank( message = "Le nom doit être renseigné")
+	@Size(max=25, message = "Valeur trop longue pour le nom : 25 car. maxi" )
+	private String			nomEnfant;
+
+	@NotBlank( message = "Le prénom doit être renseigné")
+	@Size(max=25, message = "Valeur trop longue pour le prénom : 25 car. maxi" )
+	private String			prenomEnfant;
+
+	@NotNull( message = "La date de naissance est obligatoire")
+	private Date  dateNaissance		;
 	// Constructeurs
 	
 	
@@ -40,10 +61,34 @@ public class Contrat implements Serializable {
 
 	public Contrat() {
 	}
+	
+	public Contrat(Integer idContrat, @NotNull(message = "La date est obligatoire") Date datedbcontrat,
+			@NotBlank(message = "Le prix doit être reneigné") @Min(value = 0, message = "Le prix ne peut pas être inférieur à 0") double tarifHoraire,
+			Timer nbreHeuresEntretien, double tarifRepas, Parent parent,
+			@NotNull(message = "La date est obligatoire") Date dateDeGarde, Timer heureArrivee, Timer heureDepart,
+			boolean repas,
+			@NotBlank(message = "Le nom doit être renseigné") @Size(max = 25, message = "Valeur trop longue pour le nom : 25 car. maxi") String nomEnfant,
+			@NotBlank(message = "Le prénom doit être renseigné") @Size(max = 25, message = "Valeur trop longue pour le prénom : 25 car. maxi") String prenomEnfant,
+			@NotNull(message = "La date de naissance est obligatoire") Date dateNaissance) {
+		super();
+		this.idContrat = idContrat;
+		this.datedbcontrat = datedbcontrat;
+		this.tarifHoraire = tarifHoraire;
+		this.nbreHeuresEntretien = nbreHeuresEntretien;
+		this.tarifRepas = tarifRepas;
+		this.parent = parent;
+		DateDeGarde = dateDeGarde;
+		this.heureArrivee = heureArrivee;
+		this.heureDepart = heureDepart;
+		this.repas = repas;
+		this.nomEnfant = nomEnfant;
+		this.prenomEnfant = prenomEnfant;
+		this.dateNaissance = dateNaissance;
+	}
 
-	
-	
-	
+
+
+
 	// Getters & setters
 
 /**
@@ -56,12 +101,6 @@ public class Contrat implements Serializable {
 
 
 
-	/**
-	 * @return the garder
-	 */
-	public Garder getGarder() {
-		return garder;
-	}
 
 
 
@@ -76,12 +115,146 @@ public class Contrat implements Serializable {
 
 
 
+
+
 	/**
-	 * @param garder the garder to set
+	 * @return the dateDeGarde
 	 */
-	public void setGarder(Garder garder) {
-		this.garder = garder;
+	public Date getDateDeGarde() {
+		return DateDeGarde;
 	}
+
+
+
+
+	/**
+	 * @return the heureArrivee
+	 */
+	public Timer getHeureArrivee() {
+		return heureArrivee;
+	}
+
+
+
+
+	/**
+	 * @return the heureDepart
+	 */
+	public Timer getHeureDepart() {
+		return heureDepart;
+	}
+
+
+
+
+	/**
+	 * @return the repas
+	 */
+	public boolean isRepas() {
+		return repas;
+	}
+
+
+
+
+	/**
+	 * @return the nomEnfant
+	 */
+	public String getNomEnfant() {
+		return nomEnfant;
+	}
+
+
+
+
+	/**
+	 * @return the prenomEnfant
+	 */
+	public String getPrenomEnfant() {
+		return prenomEnfant;
+	}
+
+
+
+
+	/**
+	 * @return the dateNaissance
+	 */
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+
+
+
+
+	/**
+	 * @param dateDeGarde the dateDeGarde to set
+	 */
+	public void setDateDeGarde(Date dateDeGarde) {
+		DateDeGarde = dateDeGarde;
+	}
+
+
+
+
+	/**
+	 * @param heureArrivee the heureArrivee to set
+	 */
+	public void setHeureArrivee(Timer heureArrivee) {
+		this.heureArrivee = heureArrivee;
+	}
+
+
+
+
+	/**
+	 * @param heureDepart the heureDepart to set
+	 */
+	public void setHeureDepart(Timer heureDepart) {
+		this.heureDepart = heureDepart;
+	}
+
+
+
+
+	/**
+	 * @param repas the repas to set
+	 */
+	public void setRepas(boolean repas) {
+		this.repas = repas;
+	}
+
+
+
+
+	/**
+	 * @param nomEnfant the nomEnfant to set
+	 */
+	public void setNomEnfant(String nomEnfant) {
+		this.nomEnfant = nomEnfant;
+	}
+
+
+
+
+	/**
+	 * @param prenomEnfant the prenomEnfant to set
+	 */
+	public void setPrenomEnfant(String prenomEnfant) {
+		this.prenomEnfant = prenomEnfant;
+	}
+
+
+
+
+	/**
+	 * @param dateNaissance the dateNaissance to set
+	 */
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+
 
 
 	/**
